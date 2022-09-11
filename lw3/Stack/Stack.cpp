6ed4dfe1e164b92107f273da7c86f53a35b5b4da
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-09-11 22:04:48 +0300

    Реализовал стек

--- lw3/Stack/Stack.cpp
+++ lw3/Stack/Stack.cpp
@@ -1,7 +1,84 @@
 #include <iostream>
-#define STACK_SIZE = 20
+#define STACK_SIZE 20
+
+using namespace std;
+
+class Stack
+{
+	int* stackArr;
+	int capasity;
+	int top;
+
+public:
+	Stack(int size = STACK_SIZE);
+	~Stack();
+
+	void Push(int key);
+	int Pop();
+	int GetTop();
+	bool isFull();
+	bool isEmpty();
+};
+
+Stack::Stack(int size)
+{
+	stackArr = new int[size];
+	capasity = size;
+	top = -1;
+}
+
+Stack::~Stack()
+{
+	delete[] stackArr;
+}
+
+void Stack::Push(int key)
+{
+	if (isFull())
+	{
+		puts("Стек переполнен!");
+		exit(1);
+	}
+	top++;
+	stackArr[top] = key;
+}
+
+int Stack::Pop()
+{
+	if (isEmpty())
+	{
+		puts("Стек пустой!");
+		exit(1);
+	}
+	int key = stackArr[top];
+	top--;
+	return key;
+}
+
+int Stack::GetTop()
+{
+	if (isEmpty())
+	{
+		puts("Стек пустой");
+		exit(1);
+	}
+	return stackArr[top];
+}
+
+bool Stack::isEmpty()
+{
+	return (top == -1);
+}
+
+bool Stack::isFull()
+{
+	return (top == capasity - 1);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+	Stack stack(5);
+
+	return 0;
 }
