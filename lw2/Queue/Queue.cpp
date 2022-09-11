da0d0ec62268d96049d354164801b7c381fed42f
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-09-11 18:20:43 +0300

    Реализовал очередь

---
+++ lw2/Queue/Queue.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+using namespace std;
+#define SIZE 5000
+
+class Queue
+{
+	int* arr;
+	int capasity;
+	int front;
+	int back;
+	int count;
+
+public:
+	Queue(int size = SIZE);
+	~Queue();
+
+	void Add(int item);
+	int Head();
+	int DelHead();
+	bool IsEmpty();
+	bool IsFull();
+};
+
+Queue::Queue(int size)
+{
+	arr = new int[size];
+	capasity = size;
+	front = 0;
+	back = -1;
+	count = 0;
+}
+
+Queue::~Queue()
+{
+	delete[] arr;
+}
+
+void Queue::Add(int item)
+{
+	if (IsFull())
+	{
+		puts("Переполнение очереди!");
+		exit(1);
+	}
+	back = (back + 1) % capasity;
+	arr[back] = item;
+	count++;
+}
+
+int Queue::Head()
+{
+	if (IsEmpty())
+	{
+		puts("Очередь пустая!");
+		exit(1);
+	}
+
+	return arr[front];
+}
+
+int Queue::DelHead()
+{
+	if (IsEmpty())
+	{
+		puts("Очередь пустая!");
+		exit(1);
+	}
+	int key = arr[front];
+	front = (front + 1) % capasity;
+
+	return key;
+}
+
+bool Queue::IsFull()
+{
+	return (count == capasity);
+}
+
+bool Queue::IsEmpty()
+{
+	return (count == 0);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	Queue queue(5);
+}
