54cda80e0d83ae5c623abe820be2f5b36390f5f9
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-09-24 10:01:53 +0300

    Реализовал стек

--- lw3/Stack/Stack.cpp
+++ lw3/Stack/Stack.cpp
@@ -1,84 +1,115 @@
 #include <iostream>
-#define STACK_SIZE 20
 
 using namespace std;
 
-class Stack
+struct Node
 {
-	int* stackArr;
-	int capasity;
-	int top;
-
-public:
-	Stack(int size = STACK_SIZE);
-	~Stack();
-
-	void Push(int key);
-	int Pop();
-	int GetTop();
-	bool isFull();
-	bool isEmpty();
+	char data;
+	Node* next;
 };
 
-Stack::Stack(int size)
+struct Stack
 {
-	stackArr = new int[size];
-	capasity = size;
-	top = -1;
+	int size;
+	Node* head;
+};
+
+void InitStack(Stack* S)
+{
+	S->head = new Node;
+	S->head->next = NULL;
+	S->size = 0;
 }
 
-Stack::~Stack()
+void GetHead(Stack* S)
 {
-	delete[] stackArr;
+	cout << "Верхний элемент стека: " << S->head->data << endl;
 }
 
-void Stack::Push(int key)
+void Pop(Stack* S)
 {
-	if (isFull())
+	Node* temp;
+	cout << "Верхний элемент стека: " << S->head->data << endl;
+	temp = S->head->next;
+	delete(S->head);
+	S->head = temp;
+	delete(temp);
+	S->size--;
+}
+
+void Push(Stack* S, char item)
+{
+	S->head->next = new Node();
+	S->head = S->head->next;
+	S->head->data = item;
+	S->head->next = NULL;
+	S->size++;
+}
+
+bool IsEmpty(Stack* S)
+{
+	if (S->size == 0)
 	{
-		puts("Стек переполнен!");
-		exit(1);
+		return 1;
 	}
-	top++;
-	stackArr[top] = key;
-}
-
-int Stack::Pop()
-{
-	if (isEmpty())
+	else
 	{
-		puts("Стек пустой!");
-		exit(1);
+		return 0;
 	}
-	int key = stackArr[top];
-	top--;
-	return key;
-}
-
-int Stack::GetTop()
-{
-	if (isEmpty())
-	{
-		puts("Стек пустой");
-		exit(1);
-	}
-	return stackArr[top];
-}
-
-bool Stack::isEmpty()
-{
-	return (top == -1);
-}
-
-bool Stack::isFull()
-{
-	return (top == capasity - 1);
 }
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	Stack stack(5);
+	Stack stack;
+	char answer;
+	char item;
+
+	InitStack(&stack);
+
+	do
+	{
+		puts("Введите 1, чтобы добавить элемент в стек");
+		puts("Введите 2, чтобы вывести верхний элемент и удалить его из стека");
+		puts("Введите 3, чтобы вывести верхний элемент без удаления из стека");
+		puts("Введите 0, чтобы выйти из программы");
+
+		cin >> answer;
+		switch (answer)
+		{
+		case '1':
+			cout << "Введите элемент для вставки в стек: ";
+			cin >> item;
+			Push(&stack, item);
+			break;
+		case '2':
+			if (IsEmpty(&stack))
+			{
+				puts("Стек пуст");
+			}
+			else
+			{
+				Pop(&stack);
+			}
+			break;
+		case '3':
+			if (IsEmpty(&stack))
+			{
+				puts("Стек пуст");
+			}
+			else
+			{
+				GetHead(&stack);
+			}
+			break;
+		case '0':
+			break;
+		default:
+			puts("Команда не определена");
+			break;
+		}
+
+	} while (answer != '0');
 
 	return 0;
 }
