67dc0eb8001f1e59c61cdbaca3221529733a2e3c
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-12-11 20:50:20 +0300

    Реализовал поиск компонентов связности

---
+++ lw12/ConnectedGraphComponents/ConnectedGraphComponents.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "GraphModuleInterface.h"
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int amountVert = 0, amountEdge = 0;
+	GraphType adjMatrix[MAX_VERTEX][MAX_VERTEX];
+	SBranch edgeList[MAX_EDGE];
+
+	ReadList("ListEdg.txt", amountVert, amountEdge, edgeList);
+	ConvertListToMatrix(amountVert, amountEdge, adjMatrix, edgeList);
+
+	PrintMatrix("OutputMatrix.txt", amountVert, adjMatrix);
+
+	FindComponents(amountVert, adjMatrix);
+
+    return 0;
+}
