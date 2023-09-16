ebee0a710ea32875c484a36dfa5fa0c182c54ffe
Konstantin Kalinin <konstantin.kalinin@ispringsolutions.com>
2023-09-16 22:52:42 +0300

    Настроил bash-скрипт для запуска тестов программы. Дополнил gitignore

--- iSpring/lw1/task_26.cpp
+++ iSpring/lw1/task_26.cpp
@@ -15,18 +15,60 @@
 #include <optional>
 #include <string>
 
-struct Args {
+struct Args
+{
     std::string inputFileName;
     std::string outputFileName;
 };
 
-std::optional<Args> ParseArguments(int argc, char* argv[])
+std::optional<Args> ParseArguments(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        std::cout << "Invalid argumant count" << std::endl
+                    << "Usage: task_26.exe <inputFile> <outputFile>" << std::endl;
+        return std::nullopt;
+    }
+    Args args;
+    args.inputFileName = argv[1];
+    args.outputFileName = argv[2];
 
+    return args;
 }
 
-int main(int argc, char* argv[]) {
-    system("chcp 65001");
-    std::cout << "Hello alabama!";
-    return 0;
+int CopyFileWithReplacement(std::string& inputFileName, std::string& outputFileName)
+{
+    std::ifstream inputFile;
+    inputFile.open(inputFileName);
+
+    std::ofstream outputFile;
+    outputFile.open(outputFileName);
+
+    if (!inputFile.is_open())
+    {
+        std::cout << "Failed to open " << inputFileName << " for reading" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (!outputFile.is_open())
+    {
+        std::cout << "Failed to open " << outputFileName << " for writing" << std::endl;
+        return EXIT_FAILURE;
+    }
+    outputFile << "Hello alabama!";
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+    auto args = ParseArguments(argc, argv);
+    if (!args)
+    {
+        return EXIT_FAILURE;
+    }
+
+    CopyFileWithReplacement(args->inputFileName, args->outputFileName);
+
+    return EXIT_SUCCESS;
 }
--- iSpring/lw1/tests/task_26_test.sh
+++ iSpring/lw1/tests/task_26_test.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 PROGRAM="../task_26.cpp"
-PROGRAM_OUTPUT="task_26_output.txt"
+PROGRAM_OUTPUT="test_output.txt"
 
 INPUT_1="input_1.txt"
 OUTPUT_1="output_1.txt"
 
-g++ $PROGRAM -o $PROGRAM_OUTPUT
+g++ $PROGRAM -o task_26.exe
+
+./task_26.exe $INPUT_1 $PROGRAM_OUTPUT
 
 if cmp -s "$PROGRAM_OUTPUT" "$OUTPUT_1"; then
   echo "Test 1 passed"
 else
   echo "Test 1 failed"
+  exit 1
 fi
+
+echo "All tests passed"
+exit 0
