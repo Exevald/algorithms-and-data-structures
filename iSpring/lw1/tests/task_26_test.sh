46588715bd0442428f4d1a56353f60ece10fde37
Konstantin <64555204+Exevald@users.noreply.github.com>
2023-09-25 00:59:39 +0300

    Обновил тесты задания 26

--- iSpring/lw1/tests/task_26_test.sh
+++ iSpring/lw1/tests/task_26_test.sh
@@ -6,17 +6,38 @@ PROGRAM_OUTPUT="test_output.txt"
 INPUT_1="input_1.txt"
 OUTPUT_1="output_1.txt"
 
-g++ $PROGRAM -o task_26.exe
+INPUT_2="input_2.txt"
+OUTPUT_2="output_2.txt"
 
-./task_26.exe $INPUT_1 $PROGRAM_OUTPUT
+INPUT_3="input_3.txt"
+OUTPUT_3="output_3.txt"
 
-#if cmp -s "$PROGRAM_OUTPUT" "$OUTPUT_1"; then
-#  echo "Test 1 passed"
-#else
-#  echo "Test 1 failed"
-#  exit 1
-#fi
+g++ $PROGRAM -o task_26.exe -std=c++17
 
+
+./task_26.exe $INPUT_1 $PROGRAM_OUTPUT;
+if cmp -s "$PROGRAM_OUTPUT" "$OUTPUT_1"; then
+    echo "Test 1 passed"
+else
+    echo "Test 1 failed"
+    exit 1
+fi
+
+./task_26.exe $INPUT_2 $PROGRAM_OUTPUT;
+if cmp -s "$PROGRAM_OUTPUT" "$OUTPUT_2"; then
+    echo "Test 2 passed"
+else
+    echo "Test 2 failed"
+    exit 1
+fi
+
+./task_26.exe $INPUT_3 $PROGRAM_OUTPUT;
+if cmp -s "$PROGRAM_OUTPUT" "$OUTPUT_3"; then
+    echo "Test 3 passed"
+else
+    echo "Test 3 failed"
+    exit 1
+fi
 
 echo "All tests passed"
 exit 0
